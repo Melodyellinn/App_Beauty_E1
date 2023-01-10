6c908b6ed2567fe1dbc56c4e9d69ffba63f9171b
Melody Migliaccio <welodya@gmail.com>
2023-01-10 18:09:30 +0100

    maj app5

--- appli.py
+++ appli.py
@@ -3,19 +3,18 @@ import pandas as pd
 import plotly.express as px
 
 st.set_page_config(layout = "wide")
-data = pd.read_csv("data/bquxjob_4a55ecd5_184c3af4fbe.csv")
+data = pd.read_csv("data/data_app.csv")
 st.header("National Statistics")
 page = st.sidebar.selectbox('Select page',
   ['Country data','Continent data'])
 if page == 'Country data':
-  try:
+    col1,col2 = st.columns(2)
     df_pie_country = data.copy()[data["country"].isin(["United States", "France", "Russia",
-                                                        "India", "China", "Germany", "Finland",
+                                                   "India", "China", "Germany", "Finland",
                                                         "Canada", "Netherlands", "United Kingdom"])]
     df_pie_country['country'].value_counts(ascending=False)
     fig = px.pie(df_pie_country, values='pageviews', names='country', title='Top 10 Nombre pageviews par pays')
     fig.show()
-  except Exception as e : 
-    print(e)
+    col1.plotly_chart(fig,use_container_width=True)
 else:
   st.write("Hello")
