1d05be8e6d8ab0f50fd2c8364fdae5e1fb5f17c9
Melody Migliaccio <welodya@gmail.com>
2023-01-10 18:00:47 +0100

    maj app3

--- appli.py
+++ appli.py
@@ -9,12 +9,14 @@ st.header("National Statistics")
 page = st.sidebar.selectbox('Select page',
   ['Country data','Continent data'])
 if page == 'Country data':
-  df_pie_country = data.copy()[data["country"].isin(["United States", "France", "Russia",
-                                                       "India", "China", "Germany", "Finland",
-                                                       "Canada", "Netherlands", "United Kingdom"])]
-  df_pie_country['country'].value_counts(ascending=False)
-  fig = px.pie(df_pie_country, values='pageviews', names='country', title='Top 10 Nombre pageviews par pays')
-  fig.show()
-  
+  try:
+    df_pie_country = data.copy()[data["country"].isin(["United States", "France", "Russia",
+                                                        "India", "China", "Germany", "Finland",
+                                                        "Canada", "Netherlands", "United Kingdom"])]
+    df_pie_country['country'].value_counts(ascending=False)
+    fig = px.pie(df_pie_country, values='pageviews', names='country', title='Top 10 Nombre pageviews par pays')
+    fig.show()
+  except Exception as e : 
+    print(e)
 else:
   st.write("Hello")
