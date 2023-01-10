3797fb454dddacfc28ff80da289ef4fef5dc4593
Melody Migliaccio <welodya@gmail.com>
2023-01-10 17:53:13 +0100

    maj app

--- appli.py
+++ appli.py
@@ -4,36 +4,17 @@ import plotly.express as px
 
 
 st.set_page_config(layout = "wide")
-df = pd.DataFrame(px.data.gapminder())
+data = pd.read("data/data_app.csv")
 st.header("National Statistics")
 page = st.sidebar.selectbox('Select page',
   ['Country data','Continent data'])
 if page == 'Country data':
-  ## Countries
-  clist = df['country'].unique()
-  country = st.selectbox("Select a country:",clist)
-  col1, col2 = st.columns(2)
-  fig = px.line(df[df['country'] == country], 
-    x = "year", y = "gdpPercap",title = "GDP per Capita")
- 
-  col1.plotly_chart(fig,use_container_width = True)
-  fig = px.line(df[df['country'] == country], 
-    x = "year", y = "pop",title = "Population Growth")
+  df_pie_country = data.copy()[data["country"].isin(["United States", "France", "Russia",
+                                                       "India", "China", "Germany", "Finland",
+                                                       "Canada", "Netherlands", "United Kingdom"])]
+  df_pie_country['country'].value_counts(ascending=False)
+  fig = px.pie(df_pie_country, values='pageviews', names='country', title='Top 10 Nombre pageviews par pays')
+  fig.show()
   
-  col2.plotly_chart(fig,use_container_width = True)
 else:
-  ## Continents
-  contlist = df['continent'].unique()
- 
-  continent = st.selectbox("Select a continent:",contlist)
-  col1,col2 = st.columns(2)
-  fig = px.line(df[df['continent'] == continent], 
-    x = "year", y = "gdpPercap",
-    title = "GDP per Capita",color = 'country')
-  
-  col1.plotly_chart(fig)
-  fig = px.line(df[df['continent'] == continent], 
-    x = "year", y = "pop",
-    title = "Population",color = 'country')
-  
-  col2.plotly_chart(fig, use_container_width = True)
+  st.write("Hello")
