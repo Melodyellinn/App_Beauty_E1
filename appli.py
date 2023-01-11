a04c4dcf968824a0171eb2c862c9d61fc3a14c5e
Melody Migliaccio <welodya@gmail.com>
2023-01-11 16:17:26 +0100

    maj app12

--- appli.py
+++ appli.py
@@ -33,7 +33,6 @@ if page == 'Country data':
     col2.plotly_chart(figure,use_container_width=False)
     
 else:
-  col3 = st.columns(1)
   st.write("Map World")
   df_pie_us_vs_all = data.copy()[data['country']!="(not set)"]
   df_pie_us_vs_all['country'] = df_pie_us_vs_all["country"].apply(lambda x: "United States" if x == "United States" else "other")
@@ -60,4 +59,4 @@ else:
   
   fig_map = px.scatter_mapbox(lat = data_for_map_grouped["Latitude"]['first'],lon = data_for_map_grouped["Longitude"]['first'],size=data_for_map_grouped["time_on_site"]['mean'],color=data_for_map_grouped["pageviews"]['mean'],color_continuous_scale=px.colors.sequential.Viridis,mapbox_style ='open-street-map',size_max=50,zoom=1)
   
-  col3.plotly_chart(fig_map,use_container_width=False)
+col2.plotly_chart(fig_map,use_container_width=False)
