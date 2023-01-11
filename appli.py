e18686f75be94481dc3630ff3e692498fea3c485
Melody Migliaccio <welodya@gmail.com>
2023-01-11 16:58:53 +0100

    maj app32

--- appli.py
+++ appli.py
@@ -22,15 +22,14 @@ page = st.sidebar.selectbox('Select page',
   ['Country data','Other data'])
 
 if page == 'Country data':
-    col1,col2 = st.columns(2)
+    col1,col2 = st.columns([2, 2])
     df_pie_country = data.copy()[data["country"].isin(["United States", "France", "Russia",
                                                    "India", "China", "Germany", "Finland",
                                                         "Canada", "Netherlands", "United Kingdom"])]
     df_pie_country['country'].value_counts(ascending=False)
     fig = px.pie(df_pie_country, values='pageviews', names='country', title='Top 10 Nombre pageviews par pays')
     col1.plotly_chart(fig,use_container_width=False)
-    st.markdown('##')
-    st.markdown('##')
+    
     df_unique_user_country = data.copy().groupby(['country', 
     'fullVisitorId']).count().reset_index().groupby("country").count().reset_index()[["country","fullVisitorId"]]
     df_unique_user_country = df_unique_user_country.sort_values('fullVisitorId', ascending=False).head(10)
