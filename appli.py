9f8869ed27579dab59c336d59a7c1599a1809d1a
Melody Migliaccio <welodya@gmail.com>
2023-01-16 15:53:11 +0100

    maj_app36

--- appli.py
+++ appli.py
@@ -145,26 +145,14 @@ else:
     col4, col5 = st.columns([4, 2])
     col4.subheader("Type de channel")
     #data['rgb_colors'] = data['channelGrouping'].apply(lambda x: 'rgb(255,0,0)' if x == 'value1' else ('rgb(51,138,255)' if x == 'value2' else ('rgb(51,202,255)' if x == 'value3' else 'rgb(51,255,187)')))
-    rgb_colors = []
-    for value in data['channelGrouping'].value_counts().index:
-      if value == 'value1':
-        rgb_colors.append('rgb(51,138,255)')
-      elif value == 'value2':
-        rgb_colors.append('rgb(51,202,255)')
-      elif value == 'value3':
-        rgb_colors.append('rgb(51,202,255)')
-      elif value == 'value4':
-        rgb_colors.append('rgb(51,255,187)')
-      elif value == 'value5':
-        rgb_colors.append('rgb(224,255,51)')
-      elif value == 'value6':
-        rgb_colors.append('rgb(255,216,51)')
-      elif value == 'value7':
-        rgb_colors.append('rgb(255,132,51)')
-      elif value == 'value8':
-        rgb_colors.append('rgb(248,18,18)') # red
-      else:
-        rgb_colors.append('rgb(255,255,255)') # white
+    rgb_colors = ['rgb(51,138,255)',
+                  'rgb(51,202,255)',
+                  'rgb(51,202,255)',
+                  'rgb(51,255,187)',
+                  'rgb(224,255,51)',
+                  'rgb(255,216,51)',
+                  'rgb(255,132,51)',
+                  'rgb(248,18,18)']
 
     data_bar = [go.Bar(
             x=data['channelGrouping'].value_counts().index,
