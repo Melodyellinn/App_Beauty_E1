d465e0f9528e37b6149e30ad13c75609c952e321
Melody Migliaccio <welodya@gmail.com>
2023-01-16 13:41:33 +0100

    maj_app26

--- appli.py
+++ appli.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import plotly.express as px
 import sqlite3 as sql
 from datetime import timedelta, datetime
@@ -121,7 +123,6 @@ if page == 'Country data':
                                                         "Canada", "Netherlands", "United Kingdom"])]
       df_pie_country['country'].value_counts(ascending=False)
       fig = px.pie(df_pie_country, values='pageviews', names='country',
-                   color="category",
                    title='Top 10 Nombre pageviews par pays')
       col1.plotly_chart(fig,use_container_width=False)
     else:
@@ -140,34 +141,14 @@ if page == 'Country data':
 
 ## SECOND PAGE ##    
 else:
-  with st.container():
+  #with st.container():
     col4, col5 = st.columns([4, 2])
-    col4.subheader("World Map mean pageviews by country")
-    df_pie_us_vs_all = data.copy()[data['country']!="(not set)"]
-    df_pie_us_vs_all['country'] = df_pie_us_vs_all["country"].apply(lambda x: "United States" if x == "United States" else "other")
-    df_pie_undifined_country = data.copy()
-    df_pie_undifined_country['country'] = df_pie_undifined_country["country"].apply(lambda x: "Non-définie" if x == "(not set)" else "Définie")
-    list_of_country = [[37.09024,-95.712891,"United States"],
-                    [20.593684,78.96288,"India"],
-                    [46.227638,2.213749,"France"],
-                    [35.86166,104.195397,"China"],
-                    [61.52401,105.318756,"Russia"],
-                    [51.165691,10.451526,"Germany"],
-                    [61.92411,25.748151,"Finland"],
-                    [55.378051,3.435973,"United Kingdom"],
-                    [52.132633,5.291266,"Netherlands"],
-                    [56.130366,106.346771,"Canada"]]
-    df3 = pd.DataFrame(list_of_country, columns = ["Latitude", 'Longitude', 'country'])
-    data_with_coordonate = data.copy().merge(df3,on ="country",how = "left")
-    data_for_map = data_with_coordonate[data_with_coordonate['country'] != "(not set)"]
-    data_for_map_grouped = data_for_map.groupby("country").agg({"fullVisitorId": ["count","nunique"],
-                                    "time_on_site": ["mean","sum","max","min"],
-                                    "pageviews": ['mean','sum',"max","min"],
-                                    "Latitude": ['first'],
-                                    "Longitude": ['first']})
-  
-    fig_map = px.scatter_mapbox(lat = data_for_map_grouped["Latitude"]['first'],lon = data_for_map_grouped["Longitude"]['first'],size=data_for_map_grouped["time_on_site"]['mean'],color=data_for_map_grouped["pageviews"]['mean'],color_continuous_scale=px.colors.sequential.Viridis,mapbox_style ='open-street-map',size_max=50,zoom=1)
-    col4.plotly_chart(fig_map,use_container_width=False)
+    col4.subheader("Type de channel")
+    
+    plt.figure(figsize=(15,8))
+    plot = sns.countplot(data["channelGrouping"])
+    plt.title(label='Type de canal')
+    col4.plotly_chart(plot, use_container_width=False)
     
     col5.subheader("Top predict by week")
     top_predict_by_week = df_kpi.copy()
