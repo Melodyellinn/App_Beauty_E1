99d134a04a044d87386107657a4b2f12c20c216e
Melody Migliaccio <welodya@gmail.com>
2023-02-06 18:24:01 +0100

    push app2

--- appli.py
+++ appli.py
@@ -401,7 +401,7 @@ else:
     st.plotly_chart(fig_kpi,use_container_width=False)
 
   row_7_margin_1,row_7_col_1,row_7_margin_2 = st.columns((.1,4.5,.1)) 
-  with row_6_col_1:
+  with row_7_col_1:
     st.text('Barplots prédictions des Channels & Appareils (sur trois mois)')  
   row_8_margin_1,row_8_col_1,row_8_margin_2, row_8_col_2, row_8_margin_3 = st.columns((.1,1.5,.5,1.5,.3))  
   with row_8_col_1:
