1c48c0710341e280a194c0048df1fe13f6eb2702
Melody Migliaccio <welodya@gmail.com>
2023-02-06 14:35:35 +0100

    test css appli2

--- appli.py
+++ appli.py
@@ -275,8 +275,6 @@ if page == 'Global':
   row_0_margin_1, row_0_col_1, row_0_margin_2 = st.columns((.1,4.5,.1))
   with row_0_col_1:
     st.subheader(":blue[Prédictions par semaines (Count)]:1234:")
-    st.markdown()
-  
   row_1_margin_1, row_1_col_1,row_1_margin_2 = st.columns((1.5,1,5.))
   with row_1_col_1:
     st.plotly_chart(fig_kpi,use_container_width=False)
