433051e2c46b011e54b410c2b4bfab569a679831
Melody Migliaccio <welodya@gmail.com>
2023-02-09 18:07:01 +0100

    change kpi

--- appli.py
+++ appli.py
@@ -141,7 +141,7 @@ df_for_time_yersterday_2 = df_for_time2[df_for_time2["date"].between(start_date_
 
 ################### KPI ###################
 update_layout = go.Layout(
-  width=130,
+  width=200,
   height=110,
   title = "Prédictions par semaines (Count)"
 )
